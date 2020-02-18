0856f8b605d8ec0ea63510fd5897e985a446bc84
JohnV <johndominicvillalino@outlook.com>
2020-02-18 11:21:37 +0800

    initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+  // updateBtn
+  document.getElementById("updateBtn").addEventListener("click", function() {
+
+    let  changesDone = prompt("What updates you did?");
+    let yourName = prompt("Please put your name");
+      alert('thanks ' + yourName + " for submitting!");
+
+
+
+      // window.location.href="file:///Applications/intranetlogs/index.html"
+
+  });
+
+});
